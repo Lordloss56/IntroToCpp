2e7c928f75e8ba57375567f315e571c3fc08694a
Lordloss56 <42445742+Lordloss56@users.noreply.github.com>
2018-08-22 17:54:20 -0700

    Add files via upload

---
+++ Arrays/Arrays/Source.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+
+#include "numberPrinter.h"
+void initializeToTwo(int arr[], int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		arr[i] = 2;
+	}
+}
+
+
+
+int main()
+{
+	
+	//create an int array
+	int numbers[] = {1,2,3,4,5};
+	
+	printNumbers(values, 5);
+	int sum = sumNumbers(values, 5);
+
+	initializeToTwo(numbers, 5);
+	
+	//access an array via index
+	//iterate over an array
+	for (int i = 0; i < 5; ++i)
+	{
+		numbers[i] = 0;
+	}
+
+	for (int i = 0; i < 5; ++i)
+	{
+		std::cout << numbers[i] << std::endl;
+	}
+
+	//pass arrays into functions
+	
+	// start of closed work
+
+	void printNumbers(int numbers[], int size)
+	{
+		for (int i = 0; i < size; ++i)
+			{
+				std::cout << numbers[i] << std::endl;
+			}
+	}
+	int sumNumbers(int numbers[], int size)
+	{
+		int sum = 0;
+
+			for (int i = 0; i < size; ++i);
+		{
+			sum += numbers[i];
+		}
+		return sum;
+	}
+	int smallestValue(int numbers[], int size)
+	{
+		int smallest = int_MAX;
+
+		for (int i = 0; i < size; ++i);
+		{
+			if numbers[i] < smallest)
+			{
+			smallest = numbers[i];
+			}
+		}
+		return smallest;
+	}
+
+	int largestValue(int numbers[], int size)
+	{
+		int largest = int_MIN;
+
+		for (int i = 0; i < size; ++i);
+		{
+			if numbers[i] > largest)
+			{
+			largest = numbers[i];
+			}
+		}
+		return largest;
+	}
+
+	return 0;
+}
---
+++ Arrays/Arrays/numberprinter.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void printNumbers(int numbers[], int size);
+
+int sumNumbers(int numbers[], int size);
+
+int smallestValue(int numbers[], int size);
+
+int largestValue(int numbers[], int size);
